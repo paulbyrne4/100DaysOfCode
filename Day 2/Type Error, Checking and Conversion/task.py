490a8622b417c39db735ceb290bd9791cf3b732b
Paul Byrne <129745164+paulbyrne4@users.noreply.github.com>
2025-07-28 09:34:03 +0100

    Day1/100

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,10 @@
-print("Number of letters in your name: ", + len(input("Enter your name: ")))
+print('Number of letters in your name: ', + len(input("Enter your name: ")))
+
+
+name = input("Enter your name: ")
+length = len(name)
+print(type(name))
+
+
+print(type("Number of letters in your name: " ))
+print("Number of letters in your name: " + str(length))
